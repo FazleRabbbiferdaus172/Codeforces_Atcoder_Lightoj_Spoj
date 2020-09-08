def9ee679805ddbe1c551355dd0fc0f1683b30dd
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2020-09-09 05:39:19 +0600

    Add files via upload

--- Snacktower.py
+++ Snacktower.py
@@ -2,21 +2,6 @@ n = int(input())
 
 l = [int(x) for x in input().split()]
 found = [False]*(n+1)
-rn = n
-'''
-for i in l:
-    if i == rn:
-        found.insert(0, i)
-        while len(found) != 0 and rn in found:
-            x = found.pop(found.index(rn))
-            print(x, end=" ")
-            rn -= 1
-        print("")
-
-    else:
-        print("")
-        found.insert(0, i)
-'''
 c = n
 
 for i in l:
