4161f80a8df57fad614e30b8efe8ea83d33b0fd5
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2020-11-24 01:24:17 +0600

    Add files via upload

---
+++ Board Moves.py
@@ -0,0 +1,46 @@
+import sys
+input = sys.stdin.readline
+
+
+def list_input():
+    return list(map(int, input().split()))
+
+
+def float_compare(a, b):
+    if abs(a-b) < float(1e-9):
+        return True
+    else:
+        return False
+
+
+def sub_mod(x, mod):
+    x = x % mod
+    if x < 0:
+        x += mod
+    return x
+
+
+def divisor(n):
+    i = 1
+    dv = []
+    while i*i <= n:
+        if n % i == 0:
+            dv.append(i)
+            if i*i != n:
+                dv.append(n//i)
+        i += 1
+
+    return dv
+
+
+for _ in range(int(input().strip())):
+    n = int(input().strip())
+
+    ans, j = 0, 1
+
+    for i in range(3, n+1, 2):
+        #print("ok", i**2, (i-2)**2, j,  (i**2 - (i-2)**2)*j)
+        ans += (i**2 - (i-2)**2)*j
+        #print("tesr", ans)
+        j += 1
+    print(ans)
