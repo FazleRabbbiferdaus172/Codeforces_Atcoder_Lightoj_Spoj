f2ecf72764169b2b4ef2756b592b09dd2d2bc4ab
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2020-11-30 15:29:34 +0600

    Add files via upload

---
+++ Prison Break.py
@@ -0,0 +1,51 @@
+import sys
+input = sys.stdin.readline
+
+
+def list_input():
+    return list(map(int, input().split()))
+
+
+def float_compare(a, b):
+    if abs(a-b) < float(1e-9):
+        return True
+    else:
+        return False
+
+
+def sub_mod(x, mod):
+    x = x % mod
+    if x < 0:
+        x += mod
+    return x
+
+
+def divisor(n):
+    i = 1
+    dv = []
+    while i*i <= n:
+        if n % i == 0:
+            dv.append(i)
+            if i*i != n:
+                dv.append(n//i)
+        i += 1
+
+    return dv
+
+
+def binpow(a, n):
+    ans = 1
+    # MOD =
+    while n:
+        if n & 1:
+            ans *= a
+            # ans =  (ans * a) % MOD
+        a *= a
+        # a = (a*a) % MOD
+        n >>= 1
+    return ans
+
+
+for _ in range(int(input().strip())):
+    n, m, r, c = map(int, input().split())
+    print(max(n-r+m-c, r+c-2, n-r+c-1, m-c+r-1))
