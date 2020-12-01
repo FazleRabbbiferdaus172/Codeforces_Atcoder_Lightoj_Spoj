3ba92e60022d7c54229ae58a2185d0c63cc7751d
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2020-12-01 13:42:28 +0600

    Add files via upload

---
+++ Queue at the School.py
@@ -0,0 +1,56 @@
+import sys
+input = sys.stdin.readline
+
+
+def list_input():
+    return list(map(int, input().split()))
+
+
+def float_compare(a, b):
+    if abs(a-b) < float(1e-9):
+        return True
+    else:
+        return False
+
+
+def sub_mod(x, mod):
+    x = x % mod
+    if x < 0:
+        x += mod
+    return x
+
+
+def divisor(n):
+    i = 1
+    dv = []
+    while i*i <= n:
+        if n % i == 0:
+            dv.append(i)
+            if i*i != n:
+                dv.append(n//i)
+        i += 1
+
+    return dv
+
+
+def binpow(a, n):
+    ans = 1
+    # MOD =
+    while n:
+        if n & 1:
+            ans *= a
+            #ans =  (ans * a) % MOD
+        a *= a
+        #a = (a*a) % MOD
+        n >>= 1
+    return ans
+
+
+n, t = map(int, input().split())
+l = list(input().strip())
+for i in range(t):
+    ll = l.copy()
+    for i in range(n-1):
+        if ll[i] == "B" and ll[i+1] == "G":
+            l[i], l[i+1] = l[i+1], l[i]
+print("".join(l))
