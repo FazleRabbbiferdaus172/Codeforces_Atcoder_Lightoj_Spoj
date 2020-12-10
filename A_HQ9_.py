642ca43b1599130640f8a22bed3a303dab923f12
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2020-12-11 03:12:00 +0600

    Add files via upload

--- A_HQ9_.py
+++ A_HQ9_.py
@@ -1,16 +1,6 @@
 s = input()
-h, q, n = 1000, 1000, 1000
 ans = "NO"
-if "H" in s:
-    h = s.index("H")
-if "Q" in s:
-    q = s.index("Q")
-if "9" in s:
-    n = s.index("9")
-
-xx = min(h, q, n)
-
-if xx < 1000:
+if "H" in s or "Q" in s or "9" in s:
     ans = "YES"
 
 print(ans)
