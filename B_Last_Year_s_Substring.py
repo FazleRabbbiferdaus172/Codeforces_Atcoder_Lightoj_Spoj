e716b3b9e695414c1f682d624bc21f5b870bf89b
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2020-12-16 02:33:49 +0600

    Add files via upload

--- B_Last_Year_s_Substring.py
+++ B_Last_Year_s_Substring.py
@@ -1,22 +1,22 @@
 for _ in range(int(input())):
-    n = input()
+    n = int(input())
     s = input()
     ans = "NO"
     #print(s[:4], s[-1])
-    if s[0:2] == '20':
-        ll = s[len(s)-2:]
+    if s[0:2] == '20' and s[n-2:] == "20":
+        #ll = s[len(s)-2:]
         # print(ll)
-        if '20' in ll:
-            ans = "YES"
-    if s[0] == '2':
-        ll = s[len(s) - 3:]
+        # if '20' in ll:
+        ans = "YES"
+    if s[0] == '2' and s[n-3:] == "020":
+        #ll = s[len(s) - 3:]
         # print(ll)
-        if '020' in ll:
-            ans = "YES"
-    if s[:3] == '202':
+        # if '020' in ll:
+        ans = "YES"
+    if s[:3] == '202' and s[-1] == '0':
         # print(s[-1])
-        if s[-1] == '0':
-            ans = "YES"
+        # if s[-1] == '0':
+        ans = "YES"
     if s[:4] in "2020":
         ans = "YES"
     if s[len(s) - 4:] == '2020':
