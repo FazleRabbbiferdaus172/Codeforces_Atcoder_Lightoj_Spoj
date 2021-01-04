32cc7c82f6f6a7d2462bf501d568d9fadde76474
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2021-01-05 01:18:42 +0600

    Add files via upload

---
+++ A_Cards_for_Friends.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    w, h, n = map(int, input().split())
+    count = 1
+    while w % 2 == 0:
+        w //= 2
+        count *= 2
+    while h % 2 == 0:
+        h //= 2
+        count *= 2
+    if count >= n:
+        print("YES")
+    else:
+        print("NO")
