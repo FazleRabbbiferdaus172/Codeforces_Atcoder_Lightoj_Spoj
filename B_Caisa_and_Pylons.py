fff07126d1ed0ae5f9940272866c4ddeffc10fac
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-03-26 23:36:06 +0600

    26/03/21-fixed

--- B_Caisa_and_Pylons.py
+++ B_Caisa_and_Pylons.py
@@ -6,7 +6,8 @@ h = 0
 for i in range(n):
     x = l[i] - l[i+1]
     if h + x < 0:
-        ans += abs(x)
-        x = 0
+        ans += abs(x + h)
+        h, x = 0, 0
     h += x
+    #print(l[i], l[i+1], h, ans)
 print(ans)
