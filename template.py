22b47fac0d12c367ab212061279ae6c47f704abf
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-03-30 21:54:35 +0600

    template

--- template.py
+++ template.py
@@ -53,6 +53,7 @@ def totien_fun(n):
     let, n = (p^a) * (q^b) * (r^c)  where n is a composite number and p,q,r are prime numbers
     phi(n) = n* (p-1)/p * (q-1)/q * (r - 1)/r
            = (n/(p*q*r))(p-1)(q-1)(r-1)
+    comlexity : O(sqrt(n))
     '''
     for p in prime:
         if p*p > n:
@@ -71,3 +72,22 @@ def totien_fun(n):
         ans *= n - 1
 
     return n
+
+
+def totien_fun_till_n(n):
+    '''
+    value of phi of all numbers from 1 to n
+    comlexity : O(n*ln(n))
+    '''
+    phi = [0] * (n+1)
+    for i in range(1, n+1):
+        phi += [i]
+
+    prime = prime(n)
+
+    for p in prime:
+        for j in range(p, n+1, p):
+            phi[j] /= p
+            phi[j] *= (p-1)
+
+    return phi
