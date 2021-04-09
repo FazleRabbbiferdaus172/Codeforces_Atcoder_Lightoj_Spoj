269bf7e498954d90403d501bc2a7328343b55c7c
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-04-09 21:18:39 +0600

    9/04/21 TLE

--- B_Keyboard.py
+++ B_Keyboard.py
@@ -1,3 +1,16 @@
+def hand(isS, ll):
+    global x
+    one_hand = False
+    for j in isS:
+        for i in ll:
+            dist = ((i[0]-j[0])**2 +
+                    (i[1]-j[1])**2)**.5
+            if dist <= x:
+                one_hand = True
+                return one_hand
+    return one_hand
+
+
 n, m, x = map(int, input().split())
 g = []
 keyB = {}
@@ -9,17 +22,21 @@ for i in range(n):
 
 q = input()
 s = input()
+
+for i in range(ord('a'), ord('z')+1):
+    keyB[chr(i)] = []
 for i in range(n):
     for j in range(m):
         if g[i][j] != 'S':
-            keyB[g[i][j]] = (i, j)
+            keyB[g[i][j]] += [(i, j)]
         else:
             isS += [(i, j)]
+# print(keyB)
 isS.sort()
 # print(isS)
 count = 0
 for i in s:
-    if not i.lower() in keyB:
+    if len(keyB[i.lower()]) == 0:
         print(-1)
         break
     if i.isupper() and len(isS) == 0:
@@ -27,14 +44,17 @@ for i in s:
         break
     if i.isupper() and len(isS) > 0:
         #print("FOR: ", i)
-        for j in isS:
+        '''for j in isS:
             dist = ((keyB[i.lower()][0]-j[0])**2 +
                     (keyB[i.lower()][1]-j[1])**2)**.5
-            #print(keyB[i.lower()], j, dist)
+            print(keyB[i.lower()], j, dist)
             if dist <= x:
-                # print("YES")
+                print("YES")
                 break
         else:
+            count += 1'''
+
+        if not hand(isS, keyB[i.lower()]):
             count += 1
         #print("END: ", i)
         # print()
