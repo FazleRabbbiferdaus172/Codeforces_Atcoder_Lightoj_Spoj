5fb7732f10b38ddd2bed4348f21305610a72c1b4
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-04-12 06:05:12 +0600

    12/04/21

--- C_A_B_Palindrome.py
+++ C_A_B_Palindrome.py
@@ -1,49 +1,43 @@
 for _ in range(int(input())):
     a, b = map(int, input().split())
     s = list(input())
-
-    a = a - s.count('0')
-    b = b - s.count('1')
-
+    d = {'0': a - s.count('0'), '1': b - s.count('1')}
     for i in range(len(s)//2):
-        if s[i] != '?' and s[-(i+1)] != '?':
-            continue
+        if s[i] != '?' and s[-(i+1)] != '?' and s[i] != s[-(i+1)]:
+            s = ["-1"]
+            break
         if s[i] != '?' and s[-(i+1)] == '?':
-            if s[i] == '1' and b > 0:
-                s[-(i+1)] = '1'
-                b -= 1
-            elif s[i] == '0' and a > 0:
-                s[-(i+1)] = '0'
-                a -= 1
+            s[-(i+1)] = s[i]
+            d[s[i]] -= 1
         elif s[i] == '?' and s[-(i+1)] != '?':
-            if s[-(i+1)] == '1' and b > 0:
-                s[i] = '1'
-                b -= 1
-            elif s[-(i+1)] == '0' and a > 0:
-                s[i] = '0'
-                a -= 1
-        elif s[i] == '?' and s[-(i+1)] == '?':
-            if a >= b and a > 1:
-                s[i] = '0'
-                s[-(i+1)] = '0'
-                a -= 2
-            elif b > 1:
-                s[i] = '1'
-                s[-(i+1)] = '1'
-                b -= 2
+            s[i] = s[-(i+1)]
+            d[s[i]] -= 1
+
     if len(s) % 2 == 1:
         x = len(s) // 2
         if s[x] == '?':
-            if a > 0:
-                s[x] = '0'
-                a -= 1
-            else:
+            if d['1'] % 2 == 0 and d['0'] % 2 == 0:
+                s = ["-1"]
+            elif d['1'] % 2 == 1:
                 s[x] = '1'
-                b -= 1
-    #print("a,b", a, b)
-    if '?' in s or a != 0 or b != 0:
-        print(-1)
-    elif s != s[::-1]:
-        print(-1)
-    else:
-        print("".join(s))
+                d['1'] -= 1
+            else:
+                s[x] = '0'
+                d['0'] -= 1
+
+    for i in range(len(s)//2):
+        if s[i] == '?' and s[-(i+1)] == '?' and d['1'] > 1:
+            s[i] = '1'
+            s[-(i+1)] = '1'
+            d['1'] -= 2
+        elif s[i] == '?' and s[-(i+1)] == '?' and d['0'] > 1:
+            s[i] = '0'
+            s[-(i+1)] = '0'
+            d['0'] -= 2
+        elif s[i] == '?' and s[-(i+1)] == '?' and d['0'] <= 1 and d['1'] <= 1:
+            s = ['-1']
+            break
+
+    if s.count('0') != a or s.count('1') != b:
+        s = ['-1']
+    print("".join(s))
