540d5a6ea151a195ddb5645e842f4c04c3b76d36
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-04-17 13:31:51 +0600

    17/04/21

--- SPOJ_TOE2.py
+++ SPOJ_TOE2.py
@@ -1,3 +1,4 @@
+
 def wins(s):
     global g
     count = 0
@@ -29,30 +30,29 @@ def wins(s):
     return count
 
 
-T = int(input())
+while True:
 
-for t in range(T):
-    x, o = 0, 0
+    xx = input()
+    if xx == "end":
+        break
+    x, o = xx.count('X'), xx.count('O')
     g = []
-    for _ in range(4):
-        try:
-            xx = input().strip()
-            g.append(xx)
-        except EOFError as error:
-            xx = ""
-        x += xx.count('X')
-        o += xx.count('O')
-
-    # print("*****************")
+    g.append(xx[:3])
+    g.append(xx[3:6])
+    g.append(xx[6:])
     wins_x, wins_o = wins('X'), wins('O')
+    # print(g)
+    #print(x, o)
+    #print(wins_x, wins_o)
     if x - o > 1 or x - o < 0:
-        print("no")
+        print("invalid")
     elif wins_x > 0 and x - o != 1:
-        print("no")
+        print("invalid")
     elif wins_o > 0 and x - o != 0:
-        print("no")
+        print("invalid")
     elif wins_o > 0 and wins_x > 0:
-        print("no")
+        print("invalid")
+    elif wins_o == 0 and wins_x == 0 and x+o != 9:
+        print("invalid")
     else:
-        print("yes")
-    # print("******************")
+        print("valid")
