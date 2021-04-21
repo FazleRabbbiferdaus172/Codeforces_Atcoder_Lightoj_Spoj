6e82abc06e0fc72f1b95fbb17ca604fb82f1d3ae
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-04-21 15:29:40 +0600

    21/04/21

--- B_AND_0_Sum_Big.py
+++ B_AND_0_Sum_Big.py
@@ -1,10 +1,8 @@
 for _ in range(int(input())):
     n, k = map(int, input().split())
     mod = 1000000007
-    ans = 0
-    for i in range((2**k) // 2):
-        if i == 0:
-            ans += n
-        else:
-            ans += (n * (n-1))
+    ans = 1
+    for i in range(k):
+        ans *= n
+        ans %= mod
     print(ans % mod)
