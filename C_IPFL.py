8ae685fe0277456b39f453466e012499777cdec1
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-04-25 12:57:53 +0600

    25/04/21

--- C_IPFL.py
+++ C_IPFL.py
@@ -4,10 +4,20 @@ q = int(input())
 r = 0
 for i in range(q):
     t, a, b = map(int, input().split())
-    if t == 1 and r % 2 == 0:
+    if t == 1 and not r:
         s[a-1], s[b-1] = s[b-1], s[a-1]
-    elif t == 1 and r % 2 == 1:
-        s[b-1], s[a-1] = s[a-1], s[b-1]
+    elif t == 1 and r:
+        if a <= n:
+            a += n
+        else:
+            a -= n
+        if b <= n:
+            b += n
+        else:
+            b -= n
+        s[a-1], s[b-1] = s[b-1], s[a-1]
     if t == 2:
-        r += 1
+        r ^= 1
+if r:
+    s = s[n:] + s[:n]
 print("".join(s))
