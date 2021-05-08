b34d5401f408920369509b43a16434e85c7c367e
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-05-08 23:20:57 +0600

    8/05/21 fixed c

--- C_King_s_Path.py
+++ C_King_s_Path.py
@@ -1,30 +1,30 @@
 x0, y0, x1, y1 = map(int, input().split())
 n = int(input())
-g = {}
+g = set()
 for _ in range(n):
     a, b, c = map(int, input().split())
-    if a not in g:
-        g[a] = []
-
-    g[a] += [i for i in range(b, c+1)]
-    g[a] = set(g[a])
-    g[a] = list(g[a])
+    for i in range(b, c+1):
+        g.add((a, i))
 
 moves = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, -1), (1, 1), (-1, -1), (-1, 1)]
-
+# print(g)
 q = [(x0, y0, 0)]
-visited = [(x0, y0)]
+g.remove((x0, y0))
 ans = -1
 while q:
     # print(len(q))
     temp = q.pop(0)
+    # print(temp)
     if temp[:2] == (x1, y1):
         ans = max(ans, temp[2])
 
     for move in moves:
         temp_move = (temp[0] + move[0], temp[1] + move[1], temp[2] + 1)
-        if temp_move[0] in g and temp_move[1] in g[temp_move[0]] and not temp_move[:2] in visited:
-            q += [temp_move]
-            visited += [temp_move[0:2]]
+
+        if len(g) == 0 or not temp_move[:2] in g:
+            continue
+        q += [temp_move]
+        g.remove(temp_move[:2])
+        #print("TEST", temp_move, g[temp_move[0]])
 
 print(ans)
