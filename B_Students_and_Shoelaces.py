df6a84f4c1a2b611ce0699265d34691d95f0699b
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-05-19 01:46:30 +0600

    19/05/21

--- B_Students_and_Shoelaces.py
+++ B_Students_and_Shoelaces.py
@@ -1,4 +1,4 @@
-
+'''
 def bfs(r):
     global g, vis, child
     q = [r]
@@ -21,7 +21,7 @@ def bfs(r):
             #print(temp, i, vis[i])
             child[temp] += 1
             child[i] += 1
-    # print(child)
+    print(child)
 
     if 1 in child:
         send = 1
@@ -36,10 +36,11 @@ def bfs(r):
             g[x].remove(i)
     # print(g)
     return send
-
+'''
 
 n, m = map(int, input().split())
 g = {}
+deg = [0]*(n+1)
 for i in range(1, n+1):
     g[i] = []
 
@@ -47,9 +48,11 @@ for _ in range(m):
     a, b = map(int, input().split())
     g[a] += [b]
     g[b] += [a]
+    deg[a] += 1
+    deg[b] += 1
 
 # print(g)
-vis = [0]*(n+1)
+'''vis = [0]*(n+1)
 vis[0] = [-1]
 child = [0]*(n+1)
 ans = 0
@@ -60,4 +63,21 @@ for i in range(1, n+1):
     vis[0] = [-1]
     child = [0]*(n+1)
     ans += bfs(i)
+print(ans)
+'''
+ans = 0
+for _ in range(1, n+1):
+    r = []
+    for i in range(1, n+1):
+        if deg[i] == 1:
+            r += [i]
+            deg[i] = 0
+    if not r:
+        break
+    ans += 1
+    for i in range(1, n+1):
+        if deg[i] >= 2:
+            for j in r:
+                if j in g[i]:
+                    deg[i] -= 1
 print(ans)
