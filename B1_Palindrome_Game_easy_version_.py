4e1ba09d373060b8b5de1259c110c005f37167f4
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-05-21 00:21:18 +0600

    fixed

--- B1_Palindrome_Game_easy_version_.py
+++ B1_Palindrome_Game_easy_version_.py
@@ -4,9 +4,7 @@ for i in range(int(input())):
     temp = x.copy()
     temp[n//2] = '1'
     # print(x)
-    if x.count('0') % 2 == 0 and x.count('0') > 2:
-        print("DRAW")
-    elif x.count('0') > 2 and x[n//2] == '0' and temp == temp[::-1]:
+    if x.count('0') > 2 and x[n//2] == '0' and temp == temp[::-1]:
         print("ALICE")
     else:
         print("BOB")
