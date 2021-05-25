4517bceef0919550ca4966d9e582e658a90a6396
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-05-25 07:51:26 +0600

    for youtube

--- A_Eshag_Loves_Big_Arrays.py
+++ A_Eshag_Loves_Big_Arrays.py
@@ -1,5 +1,7 @@
 for _ in range(int(input())):
     n = int(input())
     l = list(map(int, input().split()))
-    x = l.count(min(l))
-    print(n-x)
+    min_ele = min(l)
+    min_count = l.count(min_ele)
+    ans = n - min_count
+    print(ans)
--- B_Sifid_and_Strange_Subsequences.py
+++ B_Sifid_and_Strange_Subsequences.py
@@ -10,10 +10,16 @@ for _ in range(int(input())):
             ge0 += [i]
     if len(lt0) > 0:
         mxl, mnl = max(lt0), min(lt0)
+        xxx = sorted(l, reverse=True).index(mnl)
+        xxx2 = sorted(l, reverse=True).index(mxl)
         lim = abs(mxl-mnl)
         print(lim)
         xn = 0
-        for i in ge0:
-            if i <= lim:
-                xn += 1
-        print("lim=", lim, " ans=", xn+len(lt0))
+        ans = []
+        for j, i in enumerate(l):
+            if i in lt0 and not i in ans:
+                ans += [i]
+            elif i > mnl and abs(mnl -)
+            elif i == 0:
+
+        print("lim=", lim, " ans=", len(ans)+len(lt0))
