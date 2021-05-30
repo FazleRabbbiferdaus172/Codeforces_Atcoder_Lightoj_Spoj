7c400fab4bf2aac24f5cf94e49ddf15e23c3db6b
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-05-30 19:20:04 +0600

    few changes

--- leet_code_993_Cousins_in_Binary_Tree.py
+++ leet_code_993_Cousins_in_Binary_Tree.py
@@ -7,37 +7,29 @@
 class Solution:
     def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
         q = [root]
-        visited = []
-        lvl = {root.val:0}
-        x_par,y_par = None, None
+        depth = {root.val: 0}
+        x_par, y_par = None, None
         while q:
-            temp = q.pop(0)
-            if temp.val in visited:
-                continue
-            visited += [temp.val]
+            current = q.pop(0)
+            if current.left:
+                q += [current.left]
+                depth[current.left.val] = depth[current.val] + 1
+                if current.left.val == x:
+                    x_par = current.val
+                if current.left.val == y:
+                    y_par = current.val
             
-            if temp.left:
-                q += [temp.left]
-                lvl[temp.left.val] = lvl[temp.val] +1
-                if temp.left.val == x:
-                    x_par = temp.val
-                if temp.left.val == y:
-                    y_par = temp.val
-                    
-            if temp.right:
-                q += [temp.right]
-                lvl[temp.right.val] = lvl[temp.val] +1
-
-
-                if temp.right.val == x:
-                    x_par = temp.val
-                if temp.right.val == y:
-                    y_par = temp.val
-        print("x_par= ",x_par)
-        print("y_par=", y_par)
-        print(lvl)
-        
-        if lvl[x] == lvl[y] and x_par != y_par:
+            if current.right:
+                q += [current.right]
+                depth[current.right.val] = depth[current.val] + 1
+                
+                if current.right.val == x:
+                    x_par = current.val
+                if current.right.val == y:
+                    y_par = current.val
+            
+        if depth[x] == depth[y] and x_par != y_par:
             return True
         else:
             return False
+                
