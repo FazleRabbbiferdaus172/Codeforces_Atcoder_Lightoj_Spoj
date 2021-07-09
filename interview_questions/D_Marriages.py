13e8ff3df653242d7c0c4d14312bcc1c41ee1f77
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-07-09 10:42:21 +0600

    9/07/21

--- interview_questions/D_Marriages.py
+++ interview_questions/D_Marriages.py
@@ -1,9 +1,24 @@
+import numpy as np
 
-for _ in range(int(input())):
+
+def solve(i, mask):
+    global n, a, dp
+    if mask == (1 << n) - 1:
+        return 0
+
+    if dp[i][mask] != -1:
+        return dp[i][mask]
+    for j in range(n):
+        if (mask & (1 << j)) == 0:
+            dp[i][mask] = max(dp[i][mask], a[i][j]+solve(i+1, mask | (1 << j)))
+    return dp[i][mask]
+
+
+for test in range(int(input())):
     n = int(input())
-    l = []
+    a = [[0]*16 for i in range(16)]
+    dp = [[-1]*(1 << 16) for i in range(16)]
     for i in range(n):
-        l.append(list(map(int, input().split())))
-
-    ans = [0] * n
-    taken = [0] * n
+        a[i] = list(map(int, input().split()))
+    ans = solve(0, 0)
+    print("Case {}: {}".format(test+1, ans))
